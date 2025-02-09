8d5af6d4448e99a99f1e12c228840c9f60586695
Binayak Purohit <93867057+binayak-tech@users.noreply.github.com>
2025-02-09 18:44:55 +0530

    added details to data-binding component

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent {
+  title: string = 'Learning how Data Binding works in Angular';
+  paraColor: string = 'blue';
+  btnText: string = 'Hide Details';
+  classUserDetails: string = 'show';
+  users = [
+    { name: 'User1', age: 30, role: 'Developer', organization: 'TechCorp' },
+    { name: 'User2', age: 25, role: 'Designer', organization: 'Creative Inc.' },
+    { name: 'User3', age: 35, role: 'Manager', organization: 'Business Solutions' }
+  ];
 
-  
+  onToggleBtnClicked() {
+    if (this.btnText === 'Show Details') {
+      this.btnText = 'Hide Details';
+      this.classUserDetails = 'show';
+    } else {
+      this.btnText = 'Show Details';
+      this.classUserDetails = 'hide';
+    }
+  }
 }
